6b3cbc1d232a7e3442591d0e8cf55c7fdc7481db
wajidali123 <wajid.iiui1@gmail.com>
2024-08-12 16:07:52 +0400

    Update Main.java

--- src/main/java/com/spring/session/demo/controllers/Main.java
+++ src/main/java/com/spring/session/demo/controllers/Main.java
@@ -1,14 +1,12 @@
 package com.spring.session.demo.controllers;
 
-/**
- * This class is deprecated and should not be used.
- *
- * @deprecated This class is replaced by Another class which provides better functionality.
- */
 
-@Deprecated
 public class Main {
     public static void main(String[] args) {
-        // TODO: Implement this method in the future
+        add(23,55,76);
+    }
+
+    public static int add(int a, int b, int c) {
+        return a+b+c;
     }
 }
