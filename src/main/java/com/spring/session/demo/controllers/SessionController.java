0253473b8e06bc5c8d01fd02bd42493e5543e0ae
wajidali123 <wajid.iiui1@gmail.com>
2024-08-12 15:49:42 +0400

    Update SessionController.java
    comment removed

--- src/main/java/com/spring/session/demo/controllers/SessionController.java
+++ src/main/java/com/spring/session/demo/controllers/SessionController.java
@@ -1,7 +1,6 @@
 package com.spring.session.demo.controllers;
 
 import com.spring.session.demo.model.User;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +12,7 @@ public class SessionController {
 
 
     @RequestMapping(path = "/session", produces = "application/xml")
-    public List<User> getSession(HttpSession session) {
-//        System.out.println(session);
-//        session.setMaxInactiveInterval(10);
+    public List<User> getSession() {
         List<User> users = new ArrayList<User>();
         users.add(new User("1", "John Doe", "john.doe@example.com"));
         users.add(new User("2", "Jane Doe", "jane.doe@example.com"));
