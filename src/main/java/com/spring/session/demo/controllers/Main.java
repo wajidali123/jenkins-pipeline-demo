44337d55b2ad150fd8b68443e96c5996b54f88a9
wajidali123 <wajid.iiui1@gmail.com>
2024-08-13 11:29:06 +0400

    Update Main.java

--- src/main/java/com/spring/session/demo/controllers/Main.java
+++ src/main/java/com/spring/session/demo/controllers/Main.java
@@ -1,8 +1,10 @@
 package com.spring.session.demo.controllers;
 
+@Deprecated
 public class Main {
+
     public static void main(String[] args) {
-        add(23,55,76);
+
     }
 
     public static int add(int a, int b, int c) {
